316952238eaf936c58468064a0ce145a898decb6
Aasim Nadaf <nadafaasim@gmail.com>
2026-04-29 16:20:14 +0530

    feat: integrate next-transition-router and enhance layout with theme provider and navigation

---
+++ src/lib/Nav.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+
+const Nav = () => {
+  const theme = useTheme();
+  return (
+    <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50">
+      <div className="flex items-center gap-8 bg-white/50 backdrop-blur-sm border border-gray-200 rounded-lg px-6 py-3 shadow-sm">
+        <Link href="/" className="text-sm font-semibold tracking-tight">
+          Home
+        </Link>
+        <div className="w-px h-4 bg-gray-300"></div>
+        <Link href="/about" className="text-sm font-semibold tracking-tight">
+          About
+        </Link>
+
+        <Button
+          onClick={() =>
+            theme.setTheme(theme.theme === "light" ? "dark" : "light")
+          }
+          className="text-sm font-semibold tracking-tight"
+        >
+          Switch Theme
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
